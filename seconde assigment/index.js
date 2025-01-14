9283198f9c1aaeaa8fa913d91a9476c28fe8f896
Mehakshami <mehakshami11@gmail.com>
2025-01-14 08:47:46 -0800

    chapter2 assigment
    chapter2 assigment

---
+++ seconde assigment/index.js
@@ -0,0 +1,46 @@
+// Question 1: Declare a variable called username. 
+var username;
+
+// Question 2:Declare a variable called myName & assign to it a string.
+let myName = "Mehak shami";
+
+// Question 3:Write script to 
+// a) Declare a JS variable, titled message. 
+// b) Assign “Hello World” to variable message 
+// c) Display the message in alert box. solve in javascipt
+let message;
+message = "Hello World";
+alert(message);
+
+// Question 4:
+let name = "Jhone Doe";
+alert(name);
+let age = "15 years old";
+alert(age);
+let course = "Certified Mobile Application Development";
+alert(course);
+
+
+// Question 5:
+let pizza = "PIZZA\nPIZZ\nPIZ\nPI\nP";
+alert(pizza);
+
+
+// Question 6:
+let email = "example@example.com";
+alert("My email address is " + email);
+
+
+// Question 7:
+let book = "A smarter way to learn JavaScript";
+alert("I am trying to learn from the Book " + book);
+
+
+// Question 8:
+document.write("Yah! I can write HTML content through JavaScript");
+
+
+// Question 9:
+let design = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+alert(design);
+document.write(design);
